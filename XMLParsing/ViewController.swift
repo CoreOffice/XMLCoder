3925b31ce1be7ae92519da6a7156f4c2c1b1ce7e
Shawn Moore <sm5@me.com>
2017-11-15 11:50:02 -0600

    Initial Commit

---
+++ XMLParsing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XMLParsing
+//
+//  Created by Shawn Moore on 11/15/17.
+//  Copyright © 2017 Shawn Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
