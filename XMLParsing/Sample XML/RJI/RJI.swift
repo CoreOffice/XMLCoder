e86fa2cdd85b91243d04967f10b253ac9ebd2686
Shawn Moore <sm5@me.com>
2017-11-22 16:20:56 -0600

    Merge of RJI RSS Channel and RJI Generator Agent

--- XMLParsing/Sample XML/RJI/RJI.swift
+++ XMLParsing/Sample XML/RJI/RJI.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct RSS: Codable {
+struct RSS: Decodable {
     var dc: URL
     var sy: URL
     var admin: URL
@@ -17,7 +17,7 @@ struct RSS: Codable {
     var channel: Channel
 }
 
-struct Channel: Codable {
+struct Channel: Decodable {
     var title: String
     var link: URL
     var description: String
@@ -25,16 +25,57 @@ struct Channel: Codable {
     var creator: String
     var rights: String
     var date: Date
-    var generatorAgent: GeneratorAgent
+    var generatorAgentResource: URL
     var image: Image
-    var item: [Item]
+    var items: [Item]
+    
+    enum CodingKeys: String, CodingKey {
+        case title, link, description, image
+        
+        case language = "dc:language"
+        case creator = "dc:creator"
+        case rights = "dc:rights"
+        case date = "dc:date"
+        case generatorAgent = "admin:generatorAgent"
+        case items = "item"
+    }
+    
+    enum GeneratorAgentKeys: String, CodingKey {
+        case resource = "rdf:resource"
+    }
+    
+    init(title: String, link: URL, description: String, language: String, creator: String, rights: String, date: Date, generatorAgentResource: URL, image: Image, items: [Item]) {
+        self.title = title
+        self.link = link
+        self.description = description
+        self.language = language
+        self.creator = creator
+        self.rights = rights
+        self.date = date
+        self.generatorAgentResource = generatorAgentResource
+        self.image = image
+        self.items = items
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        title = try values.decode(String.self, forKey: .title)
+        link = try values.decode(URL.self, forKey: .link)
+        description = try values.decode(String.self, forKey: .description)
+        language = try values.decode(String.self, forKey: .language)
+        creator = try values.decode(String.self, forKey: .creator)
+        rights = try values.decode(String.self, forKey: .rights)
+        date = try values.decode(Date.self, forKey: .date)
+        
+        let generatorAgentValues = try values.nestedContainer(keyedBy: GeneratorAgentKeys.self, forKey: .generatorAgent)
+        generatorAgentResource = try generatorAgentValues.decode(URL.self, forKey: .resource)
+        
+        image = try values.decode(Image.self, forKey: .image)
+        items = try values.decode([Item].self, forKey: .items)
+    }
 }
 
-struct GeneratorAgent: Codable {
-    var resource: URL
-}
-
-struct Image: Codable {
+struct Image: Decodable {
     var url: URL
     var height: Int
     var width: Int
@@ -42,7 +83,7 @@ struct Image: Codable {
     var title: String
 }
 
-struct Item: Codable {
+struct Item: Decodable {
     var title: String
     var link: URL
     var GUID: URL
@@ -53,7 +94,7 @@ struct Item: Codable {
     var author: String
 }
 
-struct Enclosure: Codable {
+struct Enclosure: Decodable {
     var url: URL
     var length: String
     var type: String
--- XMLParsing/ViewController.swift
+++ XMLParsing/ViewController.swift
@@ -14,12 +14,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        parseNoteError()
+//        let dict = parseToDict(retrieveData(for: "book")!)
+        
+//        let dict = parseToDict(retrieveData(for: "breakfast")!)
+        
+        let catalog: CDCatalog! = parseCDCatalog()
+        
+        print("finished")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func retrieveData(for resource: String) -> Data? {
+        guard let fileURL = Bundle.main.url(forResource: resource, withExtension: "xml") else { return nil }
+        
+        return try? Data(contentsOf: fileURL)
     }
 
     func parseToDict(_ data: Data) -> [String: Any] {
@@ -35,8 +47,7 @@ class ViewController: UIViewController {
     }
 
     func parseNote() -> Note? {
-        guard let fileURL = Bundle.main.url(forResource: "note", withExtension: "xml"),
-            let data = try? Data(contentsOf: fileURL) else { return nil }
+        guard let data = retrieveData(for: "note") else { return nil }
         
         let decoder = XMLDecoder()
         
@@ -54,20 +65,121 @@ class ViewController: UIViewController {
     }
     
     func parseNoteError() {
-        guard let fileURL = Bundle.main.url(forResource: "note_error", withExtension: "xml"),
-            let data = try? Data(contentsOf: fileURL) else { return }
+        guard let data = retrieveData(for: "note_error") else { return }
         
         let decoder = XMLDecoder()
         
-        let note: Note?
+        do {
+            _ = try decoder.decode(Note.self, from: data)
+        } catch {
+            print(error)
+        }
+    }
+    
+    func parseBook() -> Book? {
+        guard let data = retrieveData(for: "book") else { return nil }
+        
+        let decoder = XMLDecoder()
+        
+        let formatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd"
+            return formatter
+        }()
+        
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        
+        let book: Book?
         
         do {
-            note = try decoder.decode(Note.self, from: data)
+            book = try decoder.decode(Book.self, from: data)
         } catch {
             print(error)
             
-            note = nil
+            book = nil
         }
+        
+        return book
+    }
+    
+    func parseLibrary() -> Catalog? {
+        guard let data = retrieveData(for: "books") else { return nil }
+        
+        let decoder = XMLDecoder()
+        
+        let formatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd"
+            return formatter
+        }()
+        
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        
+        let catalog: Catalog?
+        
+        do {
+            catalog = try decoder.decode(Catalog.self, from: data)
+        } catch {
+            print(error)
+            
+            catalog = nil
+        }
+        
+        return catalog
+    }
+    
+    func parseMenu() -> Menu? {
+        guard let data = retrieveData(for: "breakfast") else { return nil }
+        
+        let decoder = XMLDecoder()
+        
+        let menu: Menu?
+        
+        do {
+            menu = try decoder.decode(Menu.self, from: data)
+        } catch {
+            print(error)
+            
+            menu = nil
+        }
+        
+        return menu
+    }
+    
+    func parsePlantCatalog() -> PlantCatalog? {
+        guard let data = retrieveData(for: "plant_catalog") else { return nil }
+        
+        let decoder = XMLDecoder()
+        
+        let plantCatalog: PlantCatalog?
+        
+        do {
+            plantCatalog = try decoder.decode(PlantCatalog.self, from: data)
+        } catch {
+            print(error)
+            
+            plantCatalog = nil
+        }
+        
+        return plantCatalog
+    }
+    
+    func parseCDCatalog() -> CDCatalog? {
+        guard let data = retrieveData(for: "cd_catalog") else { return nil }
+        
+        let decoder = XMLDecoder()
+        
+        let catalog: CDCatalog?
+        
+        do {
+            catalog = try decoder.decode(CDCatalog.self, from: data)
+        } catch {
+            print(error)
+            
+            catalog = nil
+        }
+        
+        return catalog
     }
 }
 
