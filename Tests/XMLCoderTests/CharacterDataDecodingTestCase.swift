f35bb7f961a2de7281a18d8bfabcdd73c3cb0fa6
Quico Moya <me@qmoya.com>
2018-12-02 22:59:48 +0100

    Add test for nested char data

--- Tests/XMLCoderTests/CharacterDataDecodingTestCase.swift
+++ Tests/XMLCoderTests/CharacterDataDecodingTestCase.swift
@@ -29,4 +29,42 @@ class CharacterDataDecodingTestCase: DecodingTestCase {
         // then
         XCTAssertEqual(attr, Attribute(name: "Xpos", value: "0"))
     }
+
+	func testItDecodesCharDataWhenNested() {
+		// given
+		struct Item: Decodable, Equatable {
+			enum CodingKeys: String, CodingKey {
+				case id = "ID"
+				case creator = "Creator"
+				case children = "attribute"
+			}
+
+			let id: String
+			let creator: String
+			let children: [Attribute]?
+		}
+
+		struct Attribute: Decodable, Equatable {
+			enum CodingKeys: String, CodingKey {
+				case name
+				case value = "#text"
+			}
+
+			let name: String
+			let value: String?
+		}
+
+		let xml = """
+			<item ID="1542637462" Creator="Francisco Moya">
+			<attribute name="Xpos">0</attribute>
+			</item>
+			"""
+
+		// when
+		let result: Item = try! decode(xml)
+
+		// then
+		let expected = Item(id: "1542637462", creator: "Francisco Moya", children: nil)
+		XCTAssertEqual(result, expected)
+	}
 }
