8f8b29fd30ca5d9520fb5ab826e2e26acb7c671a
Vincent Esche <regexident@gmail.com>
2018-12-20 16:42:50 +0100

    Cleaned up `XMLStackParser`

--- Sources/XMLCoder/Auxiliaries/XMLStackParser.swift
+++ Sources/XMLCoder/Auxiliaries/XMLStackParser.swift
@@ -9,87 +9,92 @@
 import Foundation
 
 internal class _XMLStackParser: NSObject, XMLParserDelegate {
-    var root: _XMLElement?
-    var stack = [_XMLElement]()
-    var currentNode: _XMLElement?
+    var root: _XMLElement? = nil
+    var stack: [_XMLElement] = []
+    var currentNode: _XMLElement? = nil
 
-    var currentElementName: String?
+    var currentElementName: String? = nil
     var currentElementData = ""
 
     static func parse(with data: Data) throws -> [String: Box] {
         let parser = _XMLStackParser()
 
-        do {
-            if let node = try parser.parse(with: data) {
-                return node.flatten()
-            } else {
-                throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: [], debugDescription: "The given data could not be parsed into XML."))
-            }
-        } catch {
-            throw error
+        guard let node = try parser.parse(with: data) else {
+            throw DecodingError.dataCorrupted(DecodingError.Context(
+                codingPath: [],
+                debugDescription: "The given data could not be parsed into XML."
+            ))
         }
+        
+        return node.flatten()
     }
 
     func parse(with data: Data) throws -> _XMLElement? {
         let xmlParser = XMLParser(data: data)
         xmlParser.delegate = self
-
-        if xmlParser.parse() {
-            return root
-        } else if let error = xmlParser.parserError {
-            throw error
-        } else {
+        
+        guard xmlParser.parse() else {
+            if let error = xmlParser.parserError {
+                throw error
+            }
             return nil
         }
+        
+        return root
     }
 
     func parserDidStartDocument(_: XMLParser) {
         root = nil
-        stack = [_XMLElement]()
+        stack = []
     }
 
     func parser(_: XMLParser, didStartElement elementName: String, namespaceURI _: String?, qualifiedName _: String?, attributes attributeDict: [String: String] = [:]) {
         let node = _XMLElement(key: elementName)
         node.attributes = attributeDict
         stack.append(node)
-
-        if let currentNode = currentNode {
-            if currentNode.children[elementName] != nil {
-                currentNode.children[elementName]?.append(node)
-            } else {
-                currentNode.children[elementName] = [node]
-            }
-        }
+        
+        currentNode?.children[elementName, default: []].append(node)
+        
         currentNode = node
     }
 
     func parser(_: XMLParser, didEndElement _: String, namespaceURI _: String?, qualifiedName _: String?) {
-        if let poppedNode = stack.popLast() {
-            if let content = poppedNode.value?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines) {
-                if content.isEmpty {
-                    poppedNode.value = nil
-                } else {
-                    poppedNode.value = content
-                }
-            }
-
-            if stack.isEmpty {
-                root = poppedNode
-                currentNode = nil
-            } else {
-                currentNode = stack.last
-            }
+        guard let poppedNode = stack.popLast() else {
+            return
         }
+        
+        if let value = poppedNode.value?.trimmingCharacters(in: .whitespacesAndNewlines) {
+            poppedNode.value = value.isEmpty ? nil : value
+        }
+
+        if stack.isEmpty {
+            root = poppedNode
+        }
+        
+        currentNode = stack.last
     }
 
     func parser(_: XMLParser, foundCharacters string: String) {
-        currentNode?.value = (currentNode?.value ?? "") + string
+        guard let currentNode = currentNode else {
+            return
+        }
+        
+        var value = currentNode.value ?? ""
+        value.append(string)
+        currentNode.value = value
     }
 
     func parser(_: XMLParser, foundCDATA CDATABlock: Data) {
-        if let string = String(data: CDATABlock, encoding: .utf8) {
-            currentNode?.value = (currentNode?.value ?? "") + string
+        guard let string = String(data: CDATABlock, encoding: .utf8) else {
+            return
         }
+        guard let currentNode = currentNode else {
+            return
+        }
+        
+        var value = currentNode.value ?? ""
+        value.append(string)
+        currentNode.value = value
     }
 
     func parser(_: XMLParser, parseErrorOccurred parseError: Error) {
