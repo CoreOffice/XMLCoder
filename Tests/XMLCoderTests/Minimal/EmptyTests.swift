0f47ea2de3a94ec420ca8c8a7dd039c8b861cb98
Vincent Esche <regexident@gmail.com>
2018-12-21 21:18:59 +0100

    Fixed a bug that would cause `XMLEncoder` to throw on `Encodable`s encoding nothing

---
+++ Tests/XMLCoderTests/Minimal/EmptyTests.swift
@@ -0,0 +1,50 @@
+//
+//  EmptyTests.swift
+//  XMLCoderTests
+//
+//  Created by Vincent Esche on 12/19/18.
+//
+
+import XCTest
+@testable import XMLCoder
+
+class EmptyTests: XCTestCase {
+    struct Container: Codable, Equatable {
+        // empty
+        
+        func encode(to encoder: Encoder) throws {
+            // do nothing
+        }
+    }
+    
+    func testAttribute() throws {
+        let decoder = XMLDecoder()
+        let encoder = XMLEncoder()
+        
+        encoder.nodeEncodingStrategy = .custom { codableType, _ in
+            return { _ in .attribute }
+        }
+        
+        XCTAssertThrowsError(try decoder.decode(Container.self, from: Data()))
+        
+        let encoded = try encoder.encode(Container(), withRootKey: "container")
+        XCTAssertEqual(String(data: encoded, encoding: .utf8)!, "<container />")
+    }
+    
+    func testElement() throws {
+        let decoder = XMLDecoder()
+        let encoder = XMLEncoder()
+        
+        encoder.outputFormatting = [.prettyPrinted]
+        
+        XCTAssertThrowsError(try decoder.decode(Container.self, from: Data()))
+        
+        let encoded = try encoder.encode(Container(), withRootKey: "container")
+        XCTAssertEqual(String(data: encoded, encoding: .utf8)!, "<container />")
+    }
+    
+    static var allTests = [
+        ("testAttribute", testAttribute),
+        ("testElement", testElement),
+    ]
+}
