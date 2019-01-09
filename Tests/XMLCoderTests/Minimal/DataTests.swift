f8a4c677b960a0e0aad501b43b624dff773175bb
Hodovaniuk Matvii <matvii@hodovani.uk>
2019-01-09 16:47:37 +0200

    Add test for Decode .keyFormatted

--- Tests/XMLCoderTests/Minimal/DataTests.swift
+++ Tests/XMLCoderTests/Minimal/DataTests.swift
@@ -76,8 +76,59 @@ class DataTests: XCTestCase {
         }
     }
 
+    func testStrategy() throws {
+        let decoder = XMLDecoder()
+        let encoder = XMLEncoder()
+
+        encoder.outputFormatting = [.prettyPrinted]
+
+        for (value, xmlString) in values {
+            let xmlString =
+                """
+                <container>
+                    <value>\(xmlString)</value>
+                </container>
+                """
+            let xmlData = xmlString.data(using: .utf8)!
+
+            let decoded = try decoder.decode(Container.self, from: xmlData)
+            XCTAssertEqual(decoded.value, value)
+
+            let encoded = try encoder.encode(decoded, withRootKey: "container")
+            XCTAssertEqual(String(data: encoded, encoding: .utf8)!, xmlString)
+        }
+    }
+
+    func testKeyFormated() throws {
+        let decoder = XMLDecoder()
+        let encoder = XMLEncoder()
+
+        decoder.dataDecodingStrategy = .keyFormatted { $0.stringValue.data(using: .utf8) }
+
+        encoder.outputFormatting = [.prettyPrinted]
+
+        for (value, xmlString) in values {
+            print(value)
+            print(xmlString)
+            let xmlString =
+                """
+                <container>
+                    <value>\(xmlString)</value>
+                </container>
+                """
+            let xmlData = xmlString.data(using: .utf8)!
+
+            let decoded = try decoder.decode(Container.self, from: xmlData)
+            print(Data("value".utf8))
+            print("")
+            print(decoded.value)
+            XCTAssertEqual(decoded.value, Data("value".utf8))
+        }
+    }
+
     static var allTests = [
         ("testAttribute", testAttribute),
         ("testElement", testElement),
+        ("testKeyFormated", testKeyFormated),
     ]
 }
