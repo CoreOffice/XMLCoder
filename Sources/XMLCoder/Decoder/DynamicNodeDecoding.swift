6972ad3880a62fa3cc55e58b6f8ec97e6c351404
Max Desiatov <max@desiatov.com>
2019-03-24 18:34:16 +0000

    Improve test coverage

--- Sources/XMLCoder/Decoder/DynamicNodeDecoding.swift
+++ Sources/XMLCoder/Decoder/DynamicNodeDecoding.swift
@@ -8,15 +8,3 @@
 public protocol DynamicNodeDecoding: Encodable {
     static func nodeDecoding(for key: CodingKey) -> XMLDecoder.NodeDecoding
 }
-
-extension Array: DynamicNodeDecoding where Element: DynamicNodeDecoding {
-    public static func nodeDecoding(for key: CodingKey) -> XMLDecoder.NodeDecoding {
-        return Element.nodeDecoding(for: key)
-    }
-}
-
-extension DynamicNodeDecoding where Self: Collection, Self.Iterator.Element: DynamicNodeDecoding {
-    public static func nodeEncoding(for key: CodingKey) -> XMLDecoder.NodeDecoding {
-        return Element.nodeDecoding(for: key)
-    }
-}
