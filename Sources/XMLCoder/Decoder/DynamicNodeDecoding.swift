08350f5d5e557c2ca782ad54024162a499b33985
Max Desiatov <max@desiatov.com>
2019-03-24 19:28:39 +0000

    Add more example code to README

--- Sources/XMLCoder/Decoder/DynamicNodeDecoding.swift
+++ Sources/XMLCoder/Decoder/DynamicNodeDecoding.swift
@@ -5,6 +5,6 @@
 //  Created by Max Desiatov on 01/03/2019.
 //
 
-public protocol DynamicNodeDecoding: Encodable {
+public protocol DynamicNodeDecoding: Decodable {
     static func nodeDecoding(for key: CodingKey) -> XMLDecoder.NodeDecoding
 }
