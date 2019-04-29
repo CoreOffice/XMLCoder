c58300ecfa4f002529525464bcc1217dc2468851
Max Desiatov <max@desiatov.com>
2019-04-29 21:49:16 +0100

    Add failing test case to SpacePreserveTest

--- Tests/XMLCoderTests/SpacePreserveTest.swift
+++ Tests/XMLCoderTests/SpacePreserveTest.swift
@@ -8,17 +8,38 @@
 import XCTest
 @testable import XMLCoder
 
-private let xml = """
+private let simpleXML = """
 <t xml:space="preserve"> </t>
 """.data(using: .utf8)!
 
+private let nestedXML = """
+<sst><si><t xml:space="preserve"> </t></si></sst>
+""".data(using: .utf8)!
+
+public struct SharedStrings: Codable, Equatable {
+    public struct Item: Codable, Equatable {
+        public let text: String?
+
+        enum CodingKeys: String, CodingKey {
+            case text = "t"
+        }
+    }
+
+    public let items: [Item]
+
+    enum CodingKeys: String, CodingKey {
+        case items = "si"
+    }
+}
+
 final class SpacePreserveTest: XCTestCase {
-    func testDecoder() throws {
-        let result = try XMLDecoder().decode(String?.self, from: xml)
+    func testSimple() throws {
+        let result = try XMLDecoder().decode(String?.self, from: simpleXML)
         XCTAssertNil(result)
     }
 
-    static var allTests = [
-        ("testDecoder", testDecoder),
-    ]
+    func testNested() throws {
+        let result = try XMLDecoder().decode(SharedStrings.self, from: nestedXML)
+        XCTAssertNil(result)
+    }
 }
