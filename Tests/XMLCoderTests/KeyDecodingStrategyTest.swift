a97549375298798b8ad6bf610e6196712838018d
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2019-05-29 01:08:15 +0200

    Added tests

--- Tests/XMLCoderTests/KeyDecodingStrategyTest.swift
+++ Tests/XMLCoderTests/KeyDecodingStrategyTest.swift
@@ -12,7 +12,7 @@ private let capitalized = """
 <si><T>blah</T></si>
 """.data(using: .utf8)!
 
-private struct Item: Codable, Equatable {
+private struct CapitalizedItem: Codable, Equatable {
     public let text: String
 
     enum CodingKeys: String, CodingKey {
@@ -20,11 +20,40 @@ private struct Item: Codable, Equatable {
     }
 }
 
+private let kebabCase = """
+<si><tag-name>blah</tag-name></si>
+""".data(using: .utf8)!
+
+private struct KebabCaseItem: Codable, Equatable {
+    public let text: String
+
+    enum CodingKeys: String, CodingKey {
+        case text = "tagName"
+    }
+}
+
 final class KeyDecodingStrategyTest: XCTestCase {
     func testCapitalized() throws {
         let decoder = XMLDecoder()
         decoder.keyDecodingStrategy = .convertFromCapitalized
-        let result = try decoder.decode(Item.self, from: capitalized)
-        XCTAssertEqual(result.text, "blah")
+        let decodedResult = try decoder.decode(CapitalizedItem.self, from: capitalized)
+        XCTAssertEqual(decodedResult.text, "blah")
+
+        let encoder = XMLEncoder()
+        encoder.keyEncodingStrategy = .capitalized
+        let encodedResult = try encoder.encode(decodedResult, withRootKey: "si")
+        XCTAssertEqual(encodedResult, capitalized)
+    }
+
+    func testKebabCase() throws {
+        let decoder = XMLDecoder()
+        decoder.keyDecodingStrategy = .convertFromKebabCase
+        let decodedResult = try decoder.decode(KebabCaseItem.self, from: kebabCase)
+        XCTAssertEqual(decodedResult.text, "blah")
+
+        let encoder = XMLEncoder()
+        encoder.keyEncodingStrategy = .convertToKebabCase
+        let encodedResult = try encoder.encode(decodedResult, withRootKey: "si")
+        XCTAssertEqual(encodedResult, kebabCase)
     }
 }
