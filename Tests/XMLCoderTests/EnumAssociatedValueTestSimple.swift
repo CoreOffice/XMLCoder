9ee2b90c5ada7e166625f1f79498b52cd1622c4e
James Bean <jbean46@gmail.com>
2019-07-12 16:21:59 -0700

    Lines should not have trailing whitespace

--- Tests/XMLCoderTests/EnumAssociatedValueTestSimple.swift
+++ Tests/XMLCoderTests/EnumAssociatedValueTestSimple.swift
@@ -14,12 +14,10 @@ private enum IntOrString {
 }
 
 extension IntOrString: Decodable {
-    
     enum CodingKeys: String, CodingKey {
         case int
         case string
     }
-    
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         do {
@@ -33,21 +31,21 @@ extension IntOrString: Decodable {
 extension IntOrString: Equatable { }
 
 class EnumAssociatedValuesTest: XCTestCase {
-    
+
     func testIntOrStringIntDecoding() throws {
         let xml = "<int>42</int>"
         let result = try XMLDecoder().decode(IntOrString.self, from: xml.data(using: .utf8)!)
         let expected = IntOrString.int(42)
         XCTAssertEqual(result, expected)
     }
-    
+
     func testIntOrStringStringDecoding() throws {
         let xml = "<string>forty-two</string>"
         let result = try XMLDecoder().decode(IntOrString.self, from: xml.data(using: .utf8)!)
         let expected = IntOrString.string("forty-two")
         XCTAssertEqual(result, expected)
     }
-    
+
     func testIntOrStringArrayDecoding() throws {
         let xml = """
         <container>
