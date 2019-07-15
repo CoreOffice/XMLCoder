0040e80165a21358a49a2742cea3aa3349d1b2b4
James Bean <jbean46@gmail.com>
2019-07-15 14:41:27 -0700

    Add XMLChoiceEncodable

--- Sources/XMLCoder/Auxiliaries/XMLChoiceCodable.swift
+++ Sources/XMLCoder/Auxiliaries/XMLChoiceCodable.swift
@@ -5,4 +5,4 @@
 //  Created by James Bean on 7/15/19.
 //
 
-public protocol XMLChoiceCodable: XMLChoiceDecodable {}
+public protocol XMLChoiceCodable: XMLChoiceEncodable, XMLChoiceDecodable {}
---
+++ Sources/XMLCoder/Encoder/XMLChoiceEncodable.swift
@@ -0,0 +1,8 @@
+//
+//  XMLChoiceEncodable.swift
+//  XMLCoder
+//
+//  Created by James Bean on 7/15/19.
+//
+
+public protocol XMLChoiceEncodable {}
