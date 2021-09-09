a1064314e35e347b1471e13ba06f5d13cb1e60fe
Liam Butler-Lawrence <liamdunn@icloud.com>
2021-09-09 15:55:55 -0400

    fix(boxes): make protocols internal

--- Sources/XMLCoder/XMLPositionIndexed.swift
+++ Sources/XMLCoder/XMLPositionIndexed.swift
@@ -1,9 +1,11 @@
-public protocol XMLPositionIndexedElementProtocol { }
-public protocol XMLPositionIndexedSequenceProtocol { }
-
 public struct XMLPositionIndexed<T: Decodable>: Decodable, XMLPositionIndexedElementProtocol {
     public let index: Int
     public let value: T
 }
+
+// For type erasure
+
+protocol XMLPositionIndexedElementProtocol { }
+protocol XMLPositionIndexedSequenceProtocol { }
 
 extension Array: XMLPositionIndexedSequenceProtocol where Element: XMLPositionIndexedElementProtocol { }
