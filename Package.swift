a12606d930742bc95a4d59b6c7cde0dce8216088
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-05-27 17:03:13 +0900

    update swift version to v6

--- Package.swift
+++ Package.swift
@@ -1,19 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-#if swift(>=5.9)
-let platforms: [PackageDescription.SupportedPlatform] = [.macOS(.v10_13), .iOS(.v12), .watchOS(.v4), .tvOS(.v12)]
-#elseif swift(>=5.7)
-let platforms: [PackageDescription.SupportedPlatform] = [.macOS(.v10_13), .iOS(.v11), .watchOS(.v4), .tvOS(.v11)]
-#elseif swift(>=5.0)
-let platforms: [PackageDescription.SupportedPlatform]? = nil
-#endif
-
 let package = Package(
     name: "XMLCoder",
-    platforms: platforms,
+    platforms: [.macOS(.v10_13), .iOS(.v12), .watchOS(.v4), .tvOS(.v12)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -21,16 +13,14 @@ let package = Package(
             targets: ["XMLCoder"]
         ),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "XMLCoder",
-            dependencies: []
+            swiftSettings: [
+              .swiftLanguageMode(.v5)
+            ]
         ),
         .testTarget(
             name: "XMLCoderTests",
